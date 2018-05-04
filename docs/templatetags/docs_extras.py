be59122c7f7d224782d55ed7f753817ac61d33fd
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2018-05-04 14:40:44 +0300

    TO number rounding

--- docs/templatetags/docs_extras.py
+++ docs/templatetags/docs_extras.py
@@ -4,7 +4,10 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.filter
-def fi_nbr(value):
+def fi_nbr(value, decimals=None):
+    if decimals is not None:
+        s = "{:0.%df}" % decimals
+        value = s.format(value)
     return str(value).replace('.', ',')
 
 @register.filter
